7e5fad26a6c042f8b3e5bf0a4f41c70e16bbf707
irhamfrdz <irhamfrdz@gmail.com>
2026-08-14 00:31:13 +0700

    fix: revert username label and fix typo

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -60,7 +60,7 @@ export default function Login() {
         
         <form onSubmit={handleSubmit}>
           <div className="form-group">
-            <label htmlFor="username">namename</label>
+            <label htmlFor="username">Username</label>
             <input 
               type="text" 
               id="username"

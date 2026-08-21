21933b237e428f3594e6962b401c7b42bd5f9698
Aditkaya <adityakaya500@gmail.com>
2026-08-21 14:19:58 +0700

    fix: correct relative path to database.php in PushNotificationService

--- backend/app/Services/PushNotificationService.php
+++ backend/app/Services/PushNotificationService.php
@@ -3,7 +3,7 @@
 namespace App\Services;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/../../../config/database.php';
+require_once __DIR__ . '/../../config/database.php';
 
 use Minishlink\WebPush\WebPush;
 use Minishlink\WebPush\Subscription;
